1bcdebbdd86ffdfcd1d871d0835599a0ae44a0d4
Jos Vermoesen <info@rv.be>
2022-12-06 08:19:44 +0100

    fix info secrets.js

--- kickstart/secrets/rename me to secrets.js
+++ kickstart/secrets/rename me to secrets.js
@@ -1,5 +1,7 @@
 const metamask = 'put here the string with your 12 secrets words';
 
-const rinkeby = 'your rinkeby.infura.io https string';
+// rinkeby is deprecated since autumn 2022, use görli instead!
+// const rinkeby = 'your rinkeby.infura.io https string';
+const görli = 'your goerli.infura.io https string';
 
-module.exports = { metamask, rinkeby };
+module.exports = { metamask, görli };
